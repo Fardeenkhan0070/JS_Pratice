ff67c90cdfd95a36b5cb94a2418a727b7324f865
Fardeenkhan0070 <fardeenkhanmahar1@gmail.com>
2024-03-12 11:22:33 -0700

    Promises added to code

--- Practice_js.js
+++ Practice_js.js
@@ -1,9 +1,43 @@
 
-console.log("Before five second wait.")
+// console.log("Before five second wait.")
 
-setTimeout(() => {
-    console.log("after five second")
-}, 5000);
+// setTimeout(() => {
+//     console.log("after five second")
+// }, 5000);
 
 
-console.log("Hellow world")
+// console.log("Hellow world")
+
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+
+// function calculator(a,b ,sum){
+//     setTimeout(()=>{
+//         console.log("hellow how are you.")
+//     },2000)
+//     sum(a,b);
+// }
+
+
+// calculator(4,6,sum);
+//################################################
+
+const getPromise= ()=>{
+    return new Promise ((reslove,reject)=>{
+           console.log("Waiting")
+           reject("Some error occured")
+    })
+}
+
+
+const promise = getPromise()
+
+promise.then((res)=>{
+    console.log("Promise is "+res)
+})
+
+promise.catch((err)=>{
+    console.log("Waiting for catch.."+err)
+})
